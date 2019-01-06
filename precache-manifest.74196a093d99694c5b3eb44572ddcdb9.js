457638b5508b9a058d9d2752368bdf9ec53e7d53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-06 23:47:20 +0000

    Deploy triglian/r3pi-demo to github.com/triglian/r3pi-demo.git:gh-pages

---
+++ precache-manifest.74196a093d99694c5b3eb44572ddcdb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2f8c595897831841323",
+    "url": "/r3pi-demo/static/css/main.46f40835.chunk.css"
+  },
+  {
+    "revision": "c2f8c595897831841323",
+    "url": "/r3pi-demo/static/js/main.c2f8c595.chunk.js"
+  },
+  {
+    "revision": "a7158b8fc441eb7d8b03",
+    "url": "/r3pi-demo/static/js/1.a7158b8f.chunk.js"
+  },
+  {
+    "revision": "7d84e95df434236bbddc",
+    "url": "/r3pi-demo/static/js/runtime~main.7d84e95d.js"
+  },
+  {
+    "revision": "f4d8012b105c14ba4eb2343814b69f6d",
+    "url": "/r3pi-demo/index.html"
+  }
+];
